0151560edd1da8e329a08b212b2a47f5603236cf
Hassan El Mghari <hassan4709@gmail.com>
2023-01-24 00:25:03 -0500

    only works on some articles

--- pages/api/summarize.ts
+++ pages/api/summarize.ts
@@ -2,29 +2,58 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { parse } from "node-html-parser";
 
 type Data = {
-  name: string;
+  text: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // get this from body.req
-  const url =
-    "https://techcrunch.com/2023/01/23/spotify-cuts-6-of-its-workforce-impacting-600-people/";
+  const url = req.body.article;
 
+  if (!url) {
+    return res.status(400).json({ text: "No article in the request" });
+  }
+
+  console.log({ url });
   const response = await fetch(url, {
     method: "GET",
   });
 
   const data = await response.text();
-
-  // parse the data to extract the <body> tag with node-html-parser
   const root = parse(data);
-  const body = root.querySelector(".content");
-  const bodyText = body!.innerText
+  const body = root.querySelector(".article-content");
+  console.log({ body });
+  const text = body!.innerText
     .replace(/(\r\n|\n|\r)/gm, "")
     .replace(/(\r\t|\t|\r)/gm, "");
 
-  res.status(200).json({ name: bodyText });
+  const prompt = `Summarize the following news article in a few sentences: ${text}`;
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: false,
+    n: 1,
+  };
+
+  const openAIresponse = await fetch("https://api.openai.com/v1/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const json = await openAIresponse.json();
+
+  console.log({ json });
+
+  res.status(200).json({ text: json.choices[0].text });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,19 @@ const Home: NextPage = () => {
   const [article, setArticle] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
 
+  console.log({ article });
+  console.log({ summary });
+
   const generateSummary = async () => {
     const response = await fetch("/api/summarize", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({ article }),
     });
     const data = await response.json();
-    setSummary(data);
+    setSummary(data.text);
   };
 
   return (
@@ -32,7 +38,7 @@ const Home: NextPage = () => {
         <Link className="flex items-center space-x-3" href="/">
           <Image src="/edit.png" alt="logo" width={34} height={34} />
           <h2 className={clsx("text-3xl", poppins.className)}>
-            News summarizer.
+            <span className="text-green-500">News</span> summarizer.
           </h2>
         </Link>
         <a
@@ -45,32 +51,38 @@ const Home: NextPage = () => {
       </div>
       <main className="flex flex-col max-w-5xl mx-auto justify-center content-center mt-20 flex-1">
         <h1 className="max-w-5xl text-3xl sm:text-7xl font-bold text-center">
-          Summarize any <span className="text-orange-500">TechCrunch</span>{" "}
+          Summarize any <span className="text-green-500">TechCrunch</span>{" "}
           article in seconds
         </h1>
         <p className="text-xl sm:text-2xl mt-10 text-center">
-          Simply <span className="text-orange-500">copy and paste</span> the
+          Copy and paste any <span className="text-green-500">TechCrunch </span>
           article link below.
         </p>
         <input
           type="text"
           value={article}
           onChange={(e) => setArticle(e.target.value)}
-          className="bg-black border mx-auto sm:mt-10 mt-7 p-3 border-gray-500 rounded-lg w-3/4"
+          className="bg-black border mx-auto sm:mt-10 mt-7 p-3 border-gray-500 rounded-lg w-3/4 outline-white outline-1"
           placeholder="https://techcrunch.com/2023/01/23/spotify-cuts-6-of-its-workforce-impacting-600-people/"
         />
+        {/* TODO: Add loading logic */}
         <button
-          className="bg-orange-500 mx-auto w-1/3 sm:mt-10 mt-7 p-3 border-gray-500 rounded-2xl z-10 font-bold text-lg hover:bg-orange-400 transition"
+          className="bg-green-500 mx-auto w-1/3 sm:mt-10 mt-7 p-3 border-gray-500 rounded-2xl z-10 font-bold text-lg hover:bg-green-400 transition"
           onClick={generateSummary}
         >
           Summarize
         </button>
-        {summary}
+        {summary && (
+          <div>
+            <h2 className="text-3xl sm:text-5xl mt-12 text-center">Summary</h2>
+            <p className="mt-5 max-w-3xl mx-auto">{summary}</p>
+          </div>
+        )}
       </main>
     </div>
   );
 };
 
-// Or try one of our links below (put 3 cards here)
+// TODO: Add a try one of our links below (put 3 cards here)
 
 export default Home;
