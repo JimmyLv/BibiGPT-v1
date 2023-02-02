98bfcb95210e3f02d127a25967e4ee63274affea
Hassan El Mghari <hassan4709@gmail.com>
2023-02-02 14:41:49 -0500

    added try catch

--- pages/api/summarize.ts
+++ pages/api/summarize.ts
@@ -1,4 +1,3 @@
-import { NextRequest } from "next/server";
 import { parse } from "node-html-parser";
 import { OpenAIStream } from "../../utils/OpenAIStream";
 
@@ -6,7 +5,7 @@ export const config = {
   runtime: "edge",
 };
 
-export default async function handler(req: NextRequest) {
+export default async function handler(req: Request) {
   const { url } = (await req.json()) as {
     url?: string;
   };
@@ -15,31 +14,35 @@ export default async function handler(req: NextRequest) {
     return new Response("No prompt in the request", { status: 400 });
   }
 
-  const response = await fetch(url, {
-    method: "GET",
-  });
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+    });
 
-  const data = await response.text();
-  const root = parse(data);
-  const body = root.querySelector(".article-content");
-  const text = body!.innerText
-    .replace(/(\r\n|\n|\r)/gm, "")
-    .replace(/(\r\t|\t|\r)/gm, "");
+    const data = await response.text();
+    const root = parse(data);
+    const body = root.querySelector(".article-content");
+    const text = body!.innerText
+      .replace(/(\r\n|\n|\r)/gm, "")
+      .replace(/(\r\t|\t|\r)/gm, "");
 
-  const prompt = `Summarize the following news article in a few sentences and do not start with a period: ${text}`;
+    const prompt = `Summarize the following news article in a few sentences and do not start with a period: ${text}`;
 
-  const payload = {
-    model: "text-davinci-003",
-    prompt,
-    temperature: 0.7,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    max_tokens: 200,
-    stream: true,
-    n: 1,
-  };
+    const payload = {
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 200,
+      stream: true,
+      n: 1,
+    };
 
-  const stream = await OpenAIStream(payload);
-  return new Response(stream);
+    const stream = await OpenAIStream(payload);
+    return new Response(stream);
+  } catch (e: any) {
+    return new Response(e, { status: 500 });
+  }
 }
