2ea42d5d080840eed8ff3b63dea07c8e53e424e6
Hassan El Mghari <hassan4709@gmail.com>
2023-02-02 20:57:31 -0500

    made dynamic route work

--- README.md
+++ README.md
@@ -5,14 +5,21 @@
 - [x] Add a footer
 - [x] Maybe summarize with bullet points? Maybe I should have a toggle that toggles between this
 - [x] Fix SEO and OG image
+- [x] Make it work for featured articles – why isn't it?
+- [x] techcrunch.com, make it work with a dynamic slug can use [[...slug]].tsx
 - [ ] Fix mobile styles
-- [ ] techcrunch.com, make it work with a dynamic slug can use [[...slug]].tsx
 
 # Todos v2
 
+- [ ] Cache with upstash redis
+- [ ] Setup log drain
+- [ ] Maybe show article title and author?
+- [ ] Do a "share URL" thing
+
+# Todos v3
+
+- [ ] Create shareable links
 - [ ] Parse URL to make sure it includes techcrunch.com. new URL(string) - make sure it's valid and host name is techcrunch
-- [ ] Make it work for featured articles – why isn't it?
 - [ ] Add a "try one of these options": Add a try one of our links below (put 3 cards here)
 - [ ] Make sure to have clean URLs, remove query params
 - [ ] Maybe a slider for how long to make the summary
-- [ ] Make them sharable links and cache with upstash redis
---
+++ pages/[...slug].tsx
@@ -0,0 +1,130 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export const Home: NextPage = () => {
+  const router = useRouter();
+  const urlState = router.query.slug;
+  let url = urlState
+    ? "https://techcrunch.com/" + (urlState as string[]).join("/")
+    : "";
+  const [summary, setSummary] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [curArticle, setCurArticle] = useState<string>("");
+
+  // TODO: Figure out a better way to do this that involves using router state
+  useEffect(() => {
+    setCurArticle(url);
+    if (url.length > 0) {
+      generateSummary(false);
+    }
+  }, [url]);
+
+  const curUrl = String(curArticle.split(".com")[1]);
+  // console.log({ urlState });
+  // console.log({ curArticle });
+  // console.log({ url });
+  // console.log({ curUrl });
+
+  const generateSummary = async (changeRouter: boolean) => {
+    // The fact that this is different than curUrl will lead to race conditions
+    if (changeRouter) {
+      router.replace(curUrl); // update url
+    }
+    setLoading(true);
+    const response = await fetch("/api/summarize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setSummary((prev) => prev + chunkValue);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex flex-col max-w-5xl mx-auto pt-20 min-h-screen">
+      <Head>
+        <title>TechCrunch Summarizer</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="flex flex-col max-w-5xl mx-auto justify-center content-center mt-20 flex-1">
+        <h1 className="max-w-5xl text-3xl sm:text-7xl font-bold text-center">
+          Summarize any <span className="text-green-500">TechCrunch</span>{" "}
+          article in seconds
+        </h1>
+        <p className="text-xl sm:text-2xl mt-10 text-center">
+          Copy and paste any <span className="text-green-500">TechCrunch </span>
+          article link below.
+        </p>
+        <input
+          type="text"
+          value={curArticle}
+          onChange={(e) => setCurArticle(e.target.value)}
+          className="bg-black border mx-auto sm:mt-10 mt-7 p-3 border-gray-500 rounded-lg w-3/4 outline-white outline-1"
+          placeholder="https://techcrunch.com/2023/01/31/google-fi-customer-data-breach"
+        />
+        {!loading && (
+          <button
+            className="bg-green-500 mx-auto w-1/3 sm:mt-10 mt-7 p-3 border-gray-500 rounded-2xl z-10 font-medium text-lg hover:bg-green-400 transition"
+            onClick={() => generateSummary(true)}
+          >
+            Summarize
+          </button>
+        )}
+        {loading && (
+          <button
+            className="bg-green-500 mx-auto w-1/3 sm:mt-10 mt-7 p-3 border-gray-500 rounded-2xl z-10 font-medium text-lg hover:bg-green-400 transition"
+            onClick={() => generateSummary(true)}
+            disabled
+          >
+            ...
+          </button>
+        )}
+        {summary && (
+          <div className="mb-10">
+            <h2 className="text-3xl sm:text-5xl mt-12 text-center font-bold">
+              Summary
+            </h2>
+            <div className="mt-6 max-w-3xl mx-auto text-lg leading-7">
+              {summary.split(".").map((sentence, index) => (
+                <div key={index}>
+                  {sentence.length > 0 && (
+                    <li className="list-disc mb-2">{sentence}</li>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
