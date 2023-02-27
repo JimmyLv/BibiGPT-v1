5941835ee01f9393662c5d58cfb52da36691828f
JimmyLv <jimmy.jinglv@gmail.com>
2023-02-27 13:35:32 +0800

    feat: add animate-bounce for bilibili logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <div className="flex items-center justify-between px-3 sm:px-3">
       <a className="flex items-center space-x-3" href="https://b.jimmylv.cn">
-        <Image src="/video.png" alt="logo" width={50} height={50} />
+        <Image src="/video.png" alt="logo" className="animate-bounce" width={50} height={50} />
         <h2 className={clsx("text-lg sm:text-3xl", poppins.className)}>
           <span className="text-pink-400	">哔哩哔哩</span> BiliGPT
         </h2>
