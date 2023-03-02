fda81686903125cea02f92b93f686b00dea5f0ea
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-02 19:02:10 +0800

    fix: split the rate limit redis

--- middleware.ts
+++ middleware.ts
@@ -6,19 +6,17 @@ import { Ratelimit } from "@upstash/ratelimit";
 const redis = Redis.fromEnv();
 
 const ratelimit = new Ratelimit({
-  redis: redis,
+  redis: new Redis({
+    url: process.env.UPSTASH_RATE_REDIS_REST_URL,
+    token: process.env.UPSTASH_RATE_REDIS_REST_TOKEN,
+  }),
   // 速率限制算法 https://github.com/upstash/ratelimit#ratelimiting-algorithms
   limiter: Ratelimit.fixedWindow(5, "1 d"),
-  analytics: true // <- Enable analytics
+  analytics: true, // <- Enable analytics
 });
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { bvId, apiKey } = await req.json();
-  // TODO: unique to a user (userid, email etc) instead of IP
-  const identifier = req.ip ?? "127.0.0.3";
-  const { success, remaining } = await ratelimit.limit(identifier);
-  console.log(`======== ip ${identifier}, remaining: ${remaining} ========`);
-
   const result = await redis.get<string>(bvId);
   if (result) {
     console.log("hit cache for ", bvId);
@@ -33,6 +31,10 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     }
   }
 
+  // TODO: unique to a user (userid, email etc) instead of IP
+  const identifier = req.ip ?? "127.0.0.3";
+  const { success, remaining } = await ratelimit.limit(identifier);
+  console.log(`======== ip ${identifier}, remaining: ${remaining} ========`);
   if (!apiKey && !success) {
     return NextResponse.redirect(new URL("/blocked", req.url));
   }
