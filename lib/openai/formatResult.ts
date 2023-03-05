682d0c254961a3482f5bd26248b4b13cd09e164e
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-05 22:52:43 +0800

    feat: activate  licenseKey after used

---
+++ lib/openai/formatResult.ts
@@ -0,0 +1,7 @@
+export function formatResult(result: any) {
+  const answer = result.choices[0].message?.content || "";
+  if (answer.startsWith("\n\n")) {
+    return answer.substring(2);
+  }
+  return answer;
+}
