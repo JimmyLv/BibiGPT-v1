e43c0ec71abc39e2297b6e8c993cda8e315421b4
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-05 15:45:12 +0800

    fix: support user to enter multiple keys

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { Redis } from "@upstash/redis";
 import type { NextFetchEvent, NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { validateLicenseKey } from "./utils/3rd/lemon";
-import { checkOpenaiApiKey } from "./utils/3rd/openai";
+import { checkOpenaiApiKeys } from "./utils/3rd/openai";
 import { ratelimit } from "./utils/3rd/upstash";
 import { isDev } from "./utils/env";
 
@@ -18,7 +18,7 @@ export async function middleware(req: NextRequest, context: NextFetchEvent) {
 
   // licenseKeys
   if (apiKey) {
-    if (checkOpenaiApiKey(apiKey)) {
+    if (checkOpenaiApiKeys(apiKey)) {
       return NextResponse.next();
     }
 
---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+export default () => {
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="text-primary-600 dark:text-primary-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
+            404
+          </h1>
+          <p className="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white md:text-4xl">
+            Something's missing.
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            Sorry, we can't find that page. You'll find lots to explore on the
+            home page.{" "}
+          </p>
+          <a
+            href="#"
+            className="bg-primary-600 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:outline-none focus:ring-4"
+          >
+            Back to Homepage
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
