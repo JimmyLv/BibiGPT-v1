eb5cd64e0d1367a14ca1cf94f642787fbfe126ff
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-06 21:22:27 +0800

    refactor: extract content components

---
+++ utils/formatSummary.tsx
@@ -0,0 +1,18 @@
+import { extractSentence } from "~/utils/extractSentence";
+import { extractTimestamp } from "~/utils/extractTimestamp";
+
+export function formatSummary(summary: string) {
+  const summaryArray = summary.split("- ");
+  const formattedSummary = summaryArray
+    .map((s) => {
+      const matchResult = extractSentence(s);
+      if (matchResult) {
+        const { formattedContent, timestamp } = extractTimestamp(matchResult);
+        return timestamp + formattedContent;
+      } else {
+        return s.replace(/\n\n/g, "\n");
+      }
+    })
+    .join("\n- ");
+  return { summaryArray, formattedSummary };
+}
