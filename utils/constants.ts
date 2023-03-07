8a382ffd969f6ba38375746fad399a57f5aba4e1
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-07 21:47:45 +0800

    fix: increase free count

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,7 @@ export const BASE_DOMAIN = "https://b.jimmylv.cn";
 export const CHECKOUT_URL =
   "https://jimmylv.lemonsqueezy.com/checkout/buy/36ed8a3e-a19c-42af-aab7-46df51a14d1b";
 export const LOGIN_LIMIT_COUNT = 5;
-export const FREE_LIMIT_COUNT = 2;
+export const FREE_LIMIT_COUNT = 5;
 export const RATE_LIMIT_COUNT = LOGIN_LIMIT_COUNT + FREE_LIMIT_COUNT;
 
 export const FADE_IN_ANIMATION_SETTINGS = {
