48d52d0bb0b321cd20a132ed9caeae3f75e1f0b5
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-07 14:13:17 +0800

    fix: multiple ratelimit

--- lib/upstash.ts
+++ lib/upstash.ts
@@ -2,11 +2,19 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { FREE_LIMIT_COUNT, LOGIN_LIMIT_COUNT } from "~/utils/constants";
 
-export const ratelimit = new Ratelimit({
-  redis: new Redis({
-    url: process.env.UPSTASH_RATE_REDIS_REST_URL,
-    token: process.env.UPSTASH_RATE_REDIS_REST_TOKEN,
-  }),
+const redis = new Redis({
+  url: process.env.UPSTASH_RATE_REDIS_REST_URL,
+  token: process.env.UPSTASH_RATE_REDIS_REST_TOKEN,
+});
+
+export const ratelimitForIps = new Ratelimit({
+  redis,
+  // 速率限制算法 https://github.com/upstash/ratelimit#ratelimiting-algorithms
+  limiter: Ratelimit.fixedWindow(FREE_LIMIT_COUNT, "1 d"),
+  analytics: true, // <- Enable analytics
+});
+export const ratelimitForFreeAccounts = new Ratelimit({
+  redis,
   // 速率限制算法 https://github.com/upstash/ratelimit#ratelimiting-algorithms
   limiter: Ratelimit.fixedWindow(LOGIN_LIMIT_COUNT, "1 d"),
   analytics: true, // <- Enable analytics
