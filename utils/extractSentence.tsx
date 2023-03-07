5488bb342df122eb9d2ec2715c536450f4c9ea11
Yuiffy <yuiffy@qq.com>
2023-03-07 23:03:16 +0800

    fix: support timestamp without "." symbol

--- utils/extractSentence.tsx
+++ utils/extractSentence.tsx
@@ -1,5 +1,5 @@
 export function extractSentence(sentence: string) {
   return sentence
     .replace("0:", "0.0") // 修复第0秒
-    .match(/\s*(\d+[\.:]\d+)(.*)/);
+    .match(/^\s*(\d+[\.:]?\d+?)([:：秒 ].*)/);
 }
