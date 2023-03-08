587e48e62bdf7db754e2ca73a5fdac68074f97a1
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-08 16:35:29 +0800

    refactor: reduce timestamp logic

--- utils/reduceSubtitleTimestamp.ts
+++ utils/reduceSubtitleTimestamp.ts
@@ -4,49 +4,30 @@ export type YoutubeSubtitleItem = { start: number; lines: string[] };
 /*{ "from": 16.669, "content": "让ppt变得更加精彩" },*/
 export type BilibiliSubtitleItem = { from: number; content: string };
 
-export function reduceSubtitleTimestamp(
+export function reduceYoutubeSubtitleTimestamp(
   subtitles: Array<YoutubeSubtitleItem> = []
-): Array<CommonSubtitleItem> {
-  // 把字幕数组总共分成 20 组
-  const TOTAL_GROUP_COUNT = 20;
-  // 如果字幕不够多，就每三组合并一下
-  const MINIMUM_COUNT_ONE_GROUP = 3;
-  const eachGroupCount =
-    subtitles.length > TOTAL_GROUP_COUNT
-      ? subtitles.length / TOTAL_GROUP_COUNT
-      : MINIMUM_COUNT_ONE_GROUP;
-
-  return subtitles.reduce(
-    (
-      accumulator: CommonSubtitleItem[],
-      current: YoutubeSubtitleItem,
-      index: number
-    ) => {
-      // 计算当前元素在哪一组
-      const groupIndex: number = Math.floor(index / eachGroupCount);
-
-      // 如果是当前组的第一个元素，初始化这一组的字符串
-      if (!accumulator[groupIndex]) {
-        accumulator[groupIndex] = {
-          // 5.88 -> 5.9
-          // text: current.start.toFixed() + ": ",
-          text: current.start + ": ",
-          index: groupIndex,
-        };
-      }
-
-      // 将当前元素添加到当前组的字符串末尾
-      accumulator[groupIndex].text =
-        accumulator[groupIndex].text + current.lines.join(" ") + " ";
-
-      return accumulator;
-    },
-    []
+) {
+  return reduceSubtitleTimestamp<YoutubeSubtitleItem>(
+    subtitles,
+    (i) => i.start,
+    (i) => i.lines.join(" ")
   );
 }
+
 export function reduceBilibiliSubtitleTimestamp(
   subtitles: Array<BilibiliSubtitleItem> = []
 ): Array<CommonSubtitleItem> {
+  return reduceSubtitleTimestamp<BilibiliSubtitleItem>(
+    subtitles,
+    (i) => i.from,
+    (i) => i.content
+  );
+}
+export function reduceSubtitleTimestamp<T>(
+  subtitles: Array<T> = [],
+  getStart: (i: T) => number,
+  getText: (i: T) => string
+): Array<CommonSubtitleItem> {
   // 把字幕数组总共分成 20 组
   const TOTAL_GROUP_COUNT = 20;
   // 如果字幕不够多，就每三组合并一下
@@ -57,11 +38,7 @@ export function reduceBilibiliSubtitleTimestamp(
       : MINIMUM_COUNT_ONE_GROUP;
 
   return subtitles.reduce(
-    (
-      accumulator: CommonSubtitleItem[],
-      current: BilibiliSubtitleItem,
-      index: number
-    ) => {
+    (accumulator: CommonSubtitleItem[], current: T, index: number) => {
       // 计算当前元素在哪一组
       const groupIndex: number = Math.floor(index / eachGroupCount);
 
@@ -70,14 +47,14 @@ export function reduceBilibiliSubtitleTimestamp(
         accumulator[groupIndex] = {
           // 5.88 -> 5.9
           // text: current.start.toFixed() + ": ",
-          text: current.from + ": ",
+          text: getStart(current) + ": ",
           index: groupIndex,
         };
       }
 
       // 将当前元素添加到当前组的字符串末尾
       accumulator[groupIndex].text =
-        accumulator[groupIndex].text + current.content + " ";
+        accumulator[groupIndex].text + getText(current) + " ";
 
       return accumulator;
     },
