1fc30cbed7a82284c3bb2283f9ec5788ba2aa5f1
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-09 11:12:46 +0800

    fix: just add more menu from avatar

--- components/user-dropdown.tsx
+++ components/user-dropdown.tsx
@@ -1,6 +1,6 @@
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { motion } from "framer-motion";
-import { LayoutDashboard, LogOut } from "lucide-react";
+import { Clover, Edit, LayoutDashboard, LogOut, ShoppingBag } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 import Popover from "~/components/shared/popover";
@@ -30,11 +30,32 @@ export default function UserDropdown() {
         content={
           <div className="w-full rounded-md bg-white p-2 sm:w-56">
             <Link
-              href="/user/dashboard"
+              href="/user/videos"
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
             >
               <LayoutDashboard className="h-4 w-4" />
               <p className="text-sm">个人中心</p>
+            </Link>
+            <Link
+              href="/user/integration"
+              className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
+            >
+              <Edit className="h-4 w-4" />
+              <p className="text-sm">导出笔记</p>
+            </Link>
+            <Link
+              href="/shop"
+              className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
+            >
+              <ShoppingBag className="h-4 w-4" />
+              <p className="text-sm">购买次数</p>
+            </Link>
+            <Link
+              href="/#"
+              className="relative flex w-full cursor-not-allowed items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
+            >
+              <Clover className="h-4 w-4" />
+              <p className="text-sm">奖励计划</p>
             </Link>
             <button
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
