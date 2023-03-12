44c3002e8733a11b92aa90d57520b2b26f4cb31d
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-12 22:12:06 +0800

    feat: add mocked prompt options

---
+++ utils/constants/language.ts
@@ -0,0 +1,14 @@
+export const PROMPT_LANGUAGE_MAP: { [key: string]: string } = {
+  English: "UK English",
+  中文: "Simplified Chinese",
+  繁體中文: "Traditional Chinese",
+  日本語: "Japanese",
+  Italiano: "Italian",
+  Deutsch: "German",
+  Español: "Spanish",
+  Français: "French",
+  Nederlands: "Dutch",
+  한국어: "Korean",
+  ភាសាខ្មែរ: "Khmer",
+  हिंदी: "Hindi",
+};
