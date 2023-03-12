88ba36376c1881e936355ca970a21b7ad6e16bb0
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-12 17:03:14 +0800

    fix: just switch the checkout url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_DOMAIN = "https://b.jimmylv.cn";
 export const CHECKOUT_URL =
-  "https://jimmylv.lemonsqueezy.com/checkout/buy/36ed8a3e-a19c-42af-aab7-46df51a14d1b";
+  "https://jimmylv.lemonsqueezy.com/checkout/buy/e0c93804-abcc-47f7-848c-8756bec0e2fb";
 export const LOGIN_LIMIT_COUNT = 5;
 export const FREE_LIMIT_COUNT = 5;
 export const RATE_LIMIT_COUNT = LOGIN_LIMIT_COUNT + FREE_LIMIT_COUNT;
