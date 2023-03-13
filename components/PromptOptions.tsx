b0b62b68d84731a43c24b3d06d437b264f9f249a
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-13 11:25:28 +0800

    feat: support emoji for timestamp mode

--- components/PromptOptions.tsx
+++ components/PromptOptions.tsx
@@ -29,7 +29,6 @@ export function PromptOptions({
         <input
           type="checkbox"
           className="peer sr-only"
-          disabled={shouldShowTimestamp}
           {...register("showEmoji")}
         />
         <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-sky-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-sky-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-sky-800"></div>
