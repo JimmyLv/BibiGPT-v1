f2698200c6691a2dd337de7d66e7c4fb88d820d7
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-15 22:11:32 +0800

    fix: update input placeholder

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -192,7 +192,7 @@ export const Home: NextPage<{
           value={currentVideoUrl}
           onChange={handleInputChange}
           className="mx-auto mt-10 w-full appearance-none rounded-lg rounded-md border bg-transparent py-2 pl-2 text-sm leading-6 text-slate-900 shadow-sm ring-1 ring-slate-200 placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder={"输入 bilibili.com 视频链接，按下「回车」"}
+          placeholder={"输入 bilibili.com/youtube.com 视频链接，按下「回车」"}
         />
         <SubmitButton loading={loading} />
         <PromptOptions getValues={getValues} register={register} />
