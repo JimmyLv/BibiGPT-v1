8cc9513215668836f17ca4dc2dbd8647fcb94586
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-18 09:32:15 +0800

    feat: added internal api proxy

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
-      process.env.SUPABASE_HOSTNAME || 'xxxx.supabase.co', // to prevent vercel failed
+      process.env.SUPABASE_HOSTNAME || "xxxx.supabase.co", // to prevent vercel failed
       "b.jimmylv.cn",
       "avatars.dicebear.com",
       // "i2.hdslb.com",
@@ -18,19 +18,21 @@ module.exports = {
     ],
   },
   async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/blocked",
-          destination: "/shop",
-        },
-      ],
-    };
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.INTERNAL_API_HOSTNAME}/api/:path*`,
+      },
+      {
+        source: "/blocked",
+        destination: "/shop",
+      },
+    ];
   },
 };
 
 module.exports = withSentryConfig(
   module.exports,
   { silent: true },
-  { hideSourcemaps: true },
+  { hideSourcemaps: true }
 );
