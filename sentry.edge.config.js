4779881247d2292ae6cd78ea35351d81a06d9e86
JimmyLv <jimmy.jinglv@gmail.com>
2023-03-19 12:19:03 +0800

    ci: Revert "set sentry.properties by env"
    This reverts commit 65d32354

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: SENTRY_DSN || 'https://787cffbb40014f4d92dd9159ddf7a562@o4504790569254912.ingest.sentry.io/4504790583214080',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
