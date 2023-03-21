90af3fb7e1821c06e1e2505e08e540d501f467f3
conglinyizhi <conglinyizhi@qq.com>
2023-03-21 22:03:24 +0800

    修改飞书卡片样式

--- hooks/notes/lark.ts
+++ hooks/notes/lark.ts
@@ -2,36 +2,66 @@ import { useState } from 'react'
 import { useAnalytics } from '~/components/context/analytics'
 import { useToast } from '~/hooks/use-toast'
 
-export default function useSaveToLark(note: string, webhook: string) {
+export default function useSaveToLark(note: string, video: string, webhook: string) {
   const [loading, setLoading] = useState(false)
   const { toast } = useToast()
   const { analytics } = useAnalytics()
 
   const save = async () => {
+    const larkCardData = {
+      msg_type: 'interactive',
+      card: {
+        elements: [
+          {
+            tag: 'div',
+            text: {
+              content: note,
+              tag: 'plain_text',
+            },
+          },
+          {
+            tag: 'note',
+            elements: [
+              {
+                tag: 'plain_text',
+                content: `原视频：${video}`,
+              },
+            ],
+          },
+          {
+            tag: 'action',
+            actions: [
+              {
+                tag: 'button',
+                text: {
+                  tag: 'plain_text',
+                  content: '观看视频',
+                },
+                type: 'primary',
+                multi_url: {
+                  url: video,
+                },
+              },
+            ],
+          },
+        ],
+        header: {
+          template: 'blue',
+          title: {
+            content: 'BibiGPT 视频摘要',
+            tag: 'plain_text',
+          },
+        },
+      },
+    }
     setLoading(true)
+    console.log(note)
     const response = await fetch(webhook, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        msg_type: 'post',
-        content: {
-          post: {
-            zh_cn: {
-              title: 'BibiGPT 总结',
-              content: [
-                [
-                  {
-                    tag: 'text',
-                    text: note,
-                  },
-                ],
-              ],
-            },
-          },
-        },
-      }),
+      body: JSON.stringify(larkCardData),
     })
     const json = await response.json()
     console.log('========response========', json)
