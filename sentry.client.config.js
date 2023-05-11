760ce10da5cc0770408cbc8c8ff5535568067211
zl0662 <29133986@qq.com>
2023-05-11 17:17:29 +0800

    Update sentry.client.config.js

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://787cffbb40014f4d92dd9159ddf7a562@o4504790569254912.ingest.sentry.io/4504790583214080',
+  dsn: SENTRY_DSN || 'https://eaed882d66234ffdbae67363b1d32111@o4505163940757504.ingest.sentry.io/4505164615581696',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
